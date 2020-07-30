44bfdeaa5d9eb41a288264ba740092ad56c45210
JEFFGULICK <jeffgulick@gmail.com>
2020-07-29 23:41:15 -0500

    worked on dom.js, added method to show answers in gui

--- DOM.js
+++ DOM.js
@@ -1,6 +1,54 @@
 `use strict`
 
-const guess = () => {
-    let playGuess = document.getElementById('guess').value;
+let wordArr = ["h", "e", "l", "l", "o"];
+let resArr = [];
+
+
+const playerGuess = () => {
+    let guess = document.getElementById('guess').value;
+    document.getElementById('guess').value = ''
+    buildBoard(wordArr);
+    hangMan(guess);
+    playerGuess();
+    };
+
+const buildBoard = (array) => {
+   let board = array.map(element => '_');
+    // document.getElementById('word').innerHTML = board;
 }
 
+const checkForWin = (a, b) => {
+    for(let i = 0; i<a.length; i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+const testGuess = (guess, arr) => {
+    
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === guess){
+            resArr[i] = guess;
+            document.getElementById('word').innerHTML = resArr;
+            
+            // let newElement = document.createElement('H1');
+            // newElement.innerHTML = resArr;
+            // document.getElementById('answer').appendChild(newElement)
+        }
+    }       
+}
+
+const hangMan = (guess) => {
+    testGuess(guess, wordArr); 
+    checkForWin(guess)
+    // if(checkForWin(wordArr, resArr)){
+    //     console.log("Winner");
+    //     printBoard();
+    //     resetBoard(wordArr);
+    // }else {
+    //     playerGuess();
+    // }
+    playerGuess();
+}
