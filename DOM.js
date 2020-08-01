c9d197e9e3f6ac0b28c6c9db52ee0c97bc4d5d47
JEFFGULICK <jeffgulick@gmail.com>
2020-07-31 23:15:04 -0500

    added function to generate random words, changed the way the answers are hidden. added color gradient and more comments. project finished

--- DOM.js
+++ DOM.js
@@ -1,24 +1,34 @@
 `use strict`
 
-let wordArr = ['t', 'a', 'b', 'l', 'e'];
+let wordArr = [];
 let resArr = [];
-let wrongArr = [];
 
 //func that starts the game
 const startGame = () => {
-//    wordArr = getWord();
+    let getAWord = getWord();
+    wordArr = getAWord.split('');
     reset();
     addLetters(wordArr);
     resArr = [];
 };
+
+//func that randomly picks a word
+const getWord = () => {
+    let wordsArray = ['worm', 'puny', 'distinct', 'wash', 'prickly', 'belong', 'sturdy', 'rule', 'decisive', 'crowded'];
+    return wordsArray[Math.floor(Math.random() * 10)];
+    }
+
 //resets board
 const reset = () => {
     $( "li" ).remove();
     $("p").remove();
 }
+
 //gets player guess
 const playerGuess = () => {
     let guess = document.getElementById('guess').value;
+
+    //below checks for any input that is not a letter and returns a message. also prints out the guesses to the dom
     let check = /^[A-Za-z]+$/;
 
     if(guess.match(check)){
@@ -33,19 +43,28 @@ const playerGuess = () => {
 
     return guess;
 }
-//adds board to dom
+
+//adds board to dom and hides the word to guess
 const addLetters = (array) => {
 
     //creating li elements with letters in them
    array.map(num => {
        let letter = document.createElement('li');
-       letter.innerHTML = num;
-       letter.style.color = 'white'; //hiding the letters
+       let answer = document.createElement('P');
+       answer.innerHTML = num;
+
+       //adding class to p for later
+       $(answer).addClass('hello');
+       answer.style.visibility = 'hidden'; //hiding letter
+
+       //putting all of the pieces of the board together
        let board = document.getElementById('board');
        board.appendChild(letter);
+       letter.appendChild(answer)
     });
 }
-//this works. still need to add to dom
+
+//checks logic for win
 const checkForWin = (a, b) => {
     for(let i = 0; i<a.length; i++){
         if(a[i] != b[i]){
@@ -54,32 +73,32 @@ const checkForWin = (a, b) => {
     }
     return true;
 }
-//testing for win. revealing letters on board
+
+//testing for correct guess. revealing letters on board
 const testGuess = (guess, arr) => {
     document.getElementById('guess').value = "";
     for(let i = 0; i < arr.length; i++){
         if(arr[i] === guess){
             resArr[i] = guess;
+
             //below reveals correctly guessed letters on the board
-            let temp = document.getElementsByTagName("li").item(i);
-            temp.style.color = 'black';
+            let temp = document.getElementsByClassName('hello').item(i);
+            temp.style.visibility = 'visible'
         } 
     }       
 }
 
+//main function for game
 const hangMan = () => {
     let guess = playerGuess();
     testGuess(guess, wordArr); 
-    // checkForWin(wordArr, resArr);
-// this is for later
     if(checkForWin(wordArr, resArr)){
-    
         setTimeout(() => {
             let close = confirm("You Win!!!")
             if(close == true){
                 reset();
             }
-        }, 500);
+        }, 250);
 
         
         console.log('you winner')
