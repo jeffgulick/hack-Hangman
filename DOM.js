f7c162ab2e0589efd6b1761b95a48185c4142e0c
JEFFGULICK <jeffgulick@gmail.com>
2020-07-31 21:12:44 -0500

    mostly finished, still need to add a few things

--- DOM.js
+++ DOM.js
@@ -2,24 +2,40 @@
 
 let wordArr = ['t', 'a', 'b', 'l', 'e'];
 let resArr = [];
+let wrongArr = [];
 
 //func that starts the game
 const startGame = () => {
     // getWord();
     reset();
     addLetters(wordArr);
+    resArr = [];
 };
 //resets board
 const reset = () => {
     $( "li" ).remove();
+    $("p").remove();
 }
 //gets player guess
 const playerGuess = () => {
     let guess = document.getElementById('guess').value;
+    let check = /^[A-Za-z]+$/;
+
+    if(guess.match(check)){
+        let used = document.getElementById('usedLetters');
+        let newElement = document.createElement('P');
+        newElement.innerHTML = guess;
+        used.appendChild(newElement);    
+    } else{
+        alert('Please enter valid input: A-Z')
+    }
+
+
     return guess;
 }
 //adds board to dom
 const addLetters = (array) => {
+
     //creating li elements with letters in them
    array.map(num => {
        let letter = document.createElement('li');
@@ -44,20 +60,28 @@ const testGuess = (guess, arr) => {
     for(let i = 0; i < arr.length; i++){
         if(arr[i] === guess){
             resArr[i] = guess;
-            //below reveals correctly guessed letters
+            //below reveals correctly guessed letters on the board
             let temp = document.getElementsByTagName("li").item(i);
-            temp.style.color = 'black'
-        }
+            temp.style.color = 'black';
+        } 
     }       
 }
 
 const hangMan = () => {
     let guess = playerGuess();
     testGuess(guess, wordArr); 
-    checkForWin(wordArr, resArr);
-//this is for later
-    // if(checkForWin){
-    //     console.log('you winner')
-    //     reset();
-    // }
+    // checkForWin(wordArr, resArr);
+// this is for later
+    if(checkForWin(wordArr, resArr)){
+    
+        setTimeout(() => {
+            let close = confirm("You Win!!!")
+            if(close == true){
+                reset();
+            }
+        }, 500);
+
+        
+        console.log('you winner')
+    }
 }
